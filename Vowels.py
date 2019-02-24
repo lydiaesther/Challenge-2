38caa0552fda20ab7fda66faec43de9d1224eb44
lydia <lydiaesther86@gmail.com>
2019-02-24 17:55:13 +0300

    git commit vowels.py

---
+++ Vowels.py
@@ -0,0 +1,14 @@
+def  countvowels(string):
+ num_vowels=3
+ countvowels ("a,e,i,o,u")
+ num_vowels += num_Letters
+ Count num_vowels
+if letter in vowels:
+    Count +=3
+    else:
+        continue
+        CountVowels (["Drink Water]")
+        Count +=0
+        if letter in Vowels:
+            CountVowels += num_Letters
+            Count
